ef8f514ad03068a5cd1ce426290a17198472eb18
pedro420b <badrxxxpedro@gmail.com>
2024-07-25 02:02:32 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterfordummies/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterfordummies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+main () => runApp(const App0301());
+class App0301 extends StatelessWidget {
+  const App0301({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        
+        child: Text('Test'),
+      ),
+    );
+  }
+}
