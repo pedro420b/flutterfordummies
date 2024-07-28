d07e29f7c19552856735b77556dc04570b43174a
pedro420b <badrxxxpedro@gmail.com>
2024-07-28 20:49:44 +0100

    v00000

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
-
-
 import 'package:flutter/material.dart';
 
-main () => runApp(const App0301());
+main() => runApp(const App0301());
+
 class App0301 extends StatelessWidget {
   const App0301({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Material(
-        
-        child: Text('Test'),
-      ),
+        home: MyHomePage(),
     );
   }
 }
+
+ class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+ class _MyHomePageState extends State{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+    home: Scaffold(
+      appBar:AppBar(
+        title:const Text("appbar") 
+        ,)
+      
+      floatingActionButton: const FloatingActionButton(onPressed: (
+        
+      )),
+         ,),
+   );
+
+  }
+
+ } 
